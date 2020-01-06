a0e0a6f5fa46fd27fbd1d972cdd266b088a859d8
Nick mantia <nmantia@paradowski.com>
2020-01-05 18:41:03 -0600

    full width split

--- htdocs/app/themes/rabo_micosite/template-parts/acf-blocks/split-content/split-content.php
+++ htdocs/app/themes/rabo_micosite/template-parts/acf-blocks/split-content/split-content.php
@@ -16,7 +16,7 @@ if ( ! empty( $block['anchor'] ) ) {
 }
 
 // Create class attribute allowing for custom "className" and "align" values.
-$className = 'testimonial';
+$className = 'split-content';
 if ( ! empty( $block['className'] ) ) {
 	$className .= ' ' . $block['className'];
 }
@@ -30,6 +30,35 @@ global $acf_defaults;
 // Load values and assign defaults.
 $img_position = get_field( '50_50_image_position' ) ?: 'left';
 $img          = get_field( '50_50_image' ) ?: $acf_defaults['image'];
+$title        = get_field( '50_50_title' );
 $copy         = get_field( '50_50_content' ) ?: $acf_defaults['content'];
 $button       = get_field( '50_50_button' );
+?>
 
+<div class="<?php echo $className . ' ' . $img_position; ?>" id="<?php echo $id; ?>">
+	<?php if ( 'left' === $img_position ) : ?>
+		<div class="split-content__col">
+			<div class="split-content__image">
+				<img src="<?php echo esc_url( $img ); ?>"/>
+			</div>
+		</div>
+	<?php endif; ?>
+	<div class="split-content__col">
+		<div class="split-content__info">
+			<?php if ( $title ) : ?>
+				<h2 class="split-content__title"><?php echo $title; ?></h2>
+			<?php endif; ?>
+			<p class="split-content__content"><?php echo $copy; ?></p>
+			<?php if ( $button ) : ?>
+				<a class="button" href="<?php echo $button['url']; ?>" <?php echo $button['target'] ? $button['target'] : ''; ?>><?php echo $button['title']; ?></a>
+			<?php endif; ?>
+		</div>
+	</div>
+	<?php if ( 'right' === $img_position ) : ?>
+		<div class="split-content__col">
+			<div class="split-content__image">
+				<img src="<?php echo esc_url( $img ); ?>"/>
+			</div>
+		</div>
+	<?php endif; ?>
+</div>
--- htdocs/app/themes/rabo_micosite/template-parts/content-page.php
+++ htdocs/app/themes/rabo_micosite/template-parts/content-page.php
@@ -10,20 +10,16 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
-
-	<?php para_theme_post_thumbnail(); ?>
-
 	<div class="entry-content">
 		<?php
 		the_content();
 
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'para_theme' ),
-			'after'  => '</div>',
-		) );
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'para_theme' ),
+				'after'  => '</div>',
+			)
+		);
 		?>
 	</div><!-- .entry-content -->
 
