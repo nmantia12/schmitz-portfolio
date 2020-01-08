1622e56d3a837b50df2977052ee9df5a3b91555c
Nick mantia <nmantia@paradowski.com>
2020-01-08 15:56:14 -0600

    allow all blocks for now...

--- build/javascripts/editor/_editor.js
+++ build/javascripts/editor/_editor.js
@@ -3,34 +3,34 @@ wp.domReady(() => {
   wp.blocks.unregisterBlockStyle("core/button", "outline");
 	wp.blocks.unregisterBlockStyle("core/button", "squared");
 
-	var allowedBlocks = [
-    "core/paragraph",
-    "core/image",
-    "core/html",
-    "core/freeform",
-    "core/heading",
-    "core/list",
-    "core/button",
-    "core/separator",
-    "core/columns",
-    "core/column",
-    "acf/large-quote",
-    "acf/parallax-image",
-    "acf/fact-circle",
-    "acf/split-scroll",
-    "acf/content-image-quote",
-    "acf/infographic",
-    "acf/video-modal",
-    "acf/full-bg-img-content",
-    "acf/image-slider",
-    "acf/split-content",
-    "acf/responsive-spacer"
-	];
+	// var allowedBlocks = [
+  //   "core/paragraph",
+  //   "core/image",
+  //   "core/html",
+  //   "core/freeform",
+  //   "core/heading",
+  //   "core/list",
+  //   "core/button",
+  //   "core/separator",
+  //   "core/columns",
+  //   "core/column",
+  //   "acf/large-quote",
+  //   "acf/parallax-image",
+  //   "acf/fact-circle",
+  //   "acf/split-scroll",
+  //   "acf/content-image-quote",
+  //   "acf/infographic",
+  //   "acf/video-modal",
+  //   "acf/full-bg-img-content",
+  //   "acf/image-slider",
+  //   "acf/split-content",
+  //   "acf/responsive-spacer"
+	// ];
 
-	wp.blocks.getBlockTypes().forEach(function(blockType) {
-		if (allowedBlocks.indexOf(blockType.name) === -1) {
-			wp.blocks.unregisterBlockType(blockType.name);
-		}
-	});
+	// wp.blocks.getBlockTypes().forEach(function(blockType) {
+	// 	if (allowedBlocks.indexOf(blockType.name) === -1) {
+	// 		wp.blocks.unregisterBlockType(blockType.name);
+	// 	}
+	// });
 });
 
