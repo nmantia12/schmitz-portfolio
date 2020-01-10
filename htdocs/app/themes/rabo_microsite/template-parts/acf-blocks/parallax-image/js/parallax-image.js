52fbad796853afe23b6139b5ecca98ab0085a27f
Nick mantia <nmantia@paradowski.com>
2020-01-10 17:01:28 -0600

    adjusts parallax js enqueue

---
+++ htdocs/app/themes/rabo_microsite/template-parts/acf-blocks/parallax-image/js/parallax-image.js
@@ -0,0 +1,16 @@
+(function($) {
+  $(document).ready(function($) {
+    if ($(".parallax-image").length) {
+      // init controller
+      var controller = new ScrollMagic.Controller({
+        globalSceneOptions: { triggerHook: "onEnter", duration: "200%" }
+      });
+
+      // build scenes
+      new ScrollMagic.Scene({ triggerElement: ".parallax-image" })
+        .setTween(".parallax-image__img", { y: "-25%", ease: Linear.easeNone })
+        .addTo(controller);
+    }
+  });
+
+})(jQuery);
