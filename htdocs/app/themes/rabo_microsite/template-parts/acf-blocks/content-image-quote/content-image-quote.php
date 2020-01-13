36d50753e4d9514aa6fedc6ae9b7a7bfc9b3bb5c
Nick mantia <nmantia@paradowski.com>
2020-01-13 10:40:54 -0600

    desktop CIQ

--- htdocs/app/themes/rabo_microsite/template-parts/acf-blocks/content-image-quote/content-image-quote.php
+++ htdocs/app/themes/rabo_microsite/template-parts/acf-blocks/content-image-quote/content-image-quote.php
@@ -37,9 +37,17 @@ $CIQ_headshot = get_field( 'ciq_author_headshot' );
 ?>
 <div class="<?php echo $className; ?>" id="<?php echo $id; ?>">
 	<?php if ( $CIQ_img ) : ?>
-		<div class="ciq__img-wrap">
+		<div class="ciq__img-wrap" style="clip-path: url(#mask-<?php echo $id; ?>);
+			-webkit-clip-path: url(#mask-<?php echo $id; ?>);">
 			<img class="ciq__img" src="<?php echo esc_url( $CIQ_img ); ?>" />
 		</div>
+		<svg viewBox="0 0 1 1" preserveAspectRation="none">
+			<defs>
+				<clipPath id="mask-<?php echo $id; ?>" clipPathUnits="objectBoundingBox">
+				<path d="M 0.05 0.24 C 0.08 0.18 0.13 0.13 0.18 0.09 C 0.29 0.02 0.42 -0.01 0.55 0 C 0.67 0.01 0.8 0.04 0.88 0.14 C 0.89 0.14 0.89 0.14 0.89 0.15 C 0.97 0.23 0.99 0.37 1 0.48 C 1 0.62 0.96 0.78 0.86 0.87 C 0.78 0.95 0.67 0.98 0.56 0.99 C 0.43 1.01 0.29 1 0.18 0.91 C 0.08 0.83 0.02 0.71 0.01 0.58 C -0.01 0.46 0 0.34 0.05 0.24 Z"/>
+				</clipPath>
+			</defs>
+		</svg>
 	<?php endif; ?>
 	<?php if ( $CIQ_content ) : ?>
 		<div class="ciq__content">
@@ -54,23 +62,25 @@ $CIQ_headshot = get_field( 'ciq_author_headshot' );
 	<?php if ( $CIQ_quote ) : ?>
 		<div class="ciq__quote-wrap">
 			<h2 class="ciq__quote has-blue-color"><?php echo $CIQ_quote; ?></h2>
-			<?php if ( $CIQ_author ) : ?>
-				<h4 class="ciq__author has-grey-color"><?php echo $CIQ_author; ?></h4>
-			<?php endif; ?>
-			<?php if ( $CIQ_headshot ) : ?>
-				<svg viewBox="0 0 1 1" preserveAspectRation="none">
-					<defs>
-						<clipPath id="headshot_mask-<?php echo $id; ?>" clipPathUnits="objectBoundingBox">
-						<path d="M 0.05 0.24 C 0.08 0.18 0.13 0.13 0.18 0.09 C 0.29 0.02 0.42 -0.01 0.55 0 C 0.67 0.01 0.8 0.04 0.88 0.14 C 0.89 0.14 0.89 0.14 0.89 0.15 C 0.97 0.23 0.99 0.37 1 0.48 C 1 0.62 0.96 0.78 0.86 0.87 C 0.78 0.95 0.67 0.98 0.56 0.99 C 0.43 1.01 0.29 1 0.18 0.91 C 0.08 0.83 0.02 0.71 0.01 0.58 C -0.01 0.46 0 0.34 0.05 0.24 Z"/>
-						</clipPath>
-					</defs>
-				</svg>
-				<div style="clip-path: url(#headshot_mask-<?php echo $id; ?>);
-				-webkit-clip-path: url(#headshot_mask-<?php echo $id; ?>);" class="ciq__headshot">
-					<div class="overlay"></div>
-					<img src="<?php echo esc_url( $CIQ_headshot ); ?>"/>
-				</div>
-			<?php endif; ?>
+			<div class="ciq__author-wrap">
+				<?php if ( $CIQ_author ) : ?>
+					<h4 class="ciq__author has-grey-color"><?php echo $CIQ_author; ?></h4>
+				<?php endif; ?>
+				<?php if ( $CIQ_headshot ) : ?>
+					<svg viewBox="0 0 1 1" preserveAspectRation="none">
+						<defs>
+							<clipPath id="headshot_mask-<?php echo $id; ?>" clipPathUnits="objectBoundingBox">
+							<path d="M 0.05 0.24 C 0.08 0.18 0.13 0.13 0.18 0.09 C 0.29 0.02 0.42 -0.01 0.55 0 C 0.67 0.01 0.8 0.04 0.88 0.14 C 0.89 0.14 0.89 0.14 0.89 0.15 C 0.97 0.23 0.99 0.37 1 0.48 C 1 0.62 0.96 0.78 0.86 0.87 C 0.78 0.95 0.67 0.98 0.56 0.99 C 0.43 1.01 0.29 1 0.18 0.91 C 0.08 0.83 0.02 0.71 0.01 0.58 C -0.01 0.46 0 0.34 0.05 0.24 Z"/>
+							</clipPath>
+						</defs>
+					</svg>
+					<div style="clip-path: url(#headshot_mask-<?php echo $id; ?>);
+					-webkit-clip-path: url(#headshot_mask-<?php echo $id; ?>);" class="ciq__headshot">
+						<div class="overlay"></div>
+						<img src="<?php echo esc_url( $CIQ_headshot ); ?>"/>
+					</div>
+				<?php endif; ?>
+			</div>
 		</div>
 	<?php endif; ?>
 </div>
