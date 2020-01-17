062e1a65ea6b1a3245a009a60d36ded1718eb67b
Nick mantia <nmantia@paradowski.com>
2020-01-17 09:42:27 -0600

    allow all paradowski blocks

--- build/javascripts/editor/_editor.js
+++ build/javascripts/editor/_editor.js
@@ -1,22 +1,22 @@
 wp.domReady(() => {
-  wp.blocks.unregisterBlockStyle("core/button", "default");
-  wp.blocks.unregisterBlockStyle("core/button", "outline");
+	wp.blocks.unregisterBlockStyle("core/button", "default");
+	wp.blocks.unregisterBlockStyle("core/button", "outline");
 	wp.blocks.unregisterBlockStyle("core/button", "squared");
 
 	var allowedBlocks = [
-    "core/paragraph",
-    "core/image",
-    "core/html",
-    "core/freeform",
-    "core/heading",
-    "core/list",
-    "core/separator",
-    "core/columns",
-    "core/column",
+		"core/paragraph",
+		"core/image",
+		"core/html",
+		"core/freeform",
+		"core/heading",
+		"core/list",
+		"core/separator",
+		"core/columns",
+		"core/column"
 	];
 
-	wp.blocks.getBlockTypes().forEach(function(blockType) {
-		if (allowedBlocks.indexOf(blockType.name) === -1) {
+	wp.blocks.getBlockTypes().forEach(function (blockType) {
+		if (allowedBlocks.indexOf(blockType.name) === -1 && blockType.category != 'paradowski') {
 			wp.blocks.unregisterBlockType(blockType.name);
 		}
 	});
